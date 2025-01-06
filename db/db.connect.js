623e3162c3df3a6e9b82216617570e3e675bd6e6
Baishnawi Agrawal <baishnawiagrawal08@gmail.com>
2025-01-06 16:15:56 +0545

    Initial commit

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,19 +1,15 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
-const mongoURI = process.env.MONGODB;
+const mongoUri = process.env.MONGODB;
 
 const initializeDatabase = async () => {
-  try {
-    const connection = await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    if (connection) {
-      console.log("Connected Successfully");
-    }
-  } catch (error) {
-    console.log("Connection Failed", error);
-  }
-};
+  await mongoose
+   .connect(mongoUri)
+   .then(() => {
+     console.log("Connected to Database");
+   })
+   .catch((error) => console.log("Connection Failed", error));
+}
 
-module.exports = { initializeDatabase };
+module.exports = { initializeDatabase };
